a16c5e2450d40df7303e11a9176ad8a0ff10b707
andrecee.tnn <andrecee.tnn@gmail.com>
2020-05-25 13:15:48 +0700

    finish lab

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./App.css";
 import ChoiceCard from "./components/ChoiceCard";
 import List from "./components/List";
-
+const choices = {
+  rock: "https://www.clipartmax.com/png/middle/356-3561104_cartoon-rock-paper-scissors.png",
+  paper: "https://www.clipartmax.com/png/middle/149-1498962_cartoon-emoji-paper-rock-scissors-icon-rock-paper-scissors-emoji.png",
+  scissor: "https://www.clipartmax.com/png/middle/151-1517270_scissors-cartoon-cartoon-rock-paper-scissors.png"
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rock Paper Scissor</h1>
         <div className='container'>
-        <ChoiceCard name='Computer' color="red" />
-        <ChoiceCard name='Player' color="white" />
+        <ChoiceCard title='Computer' color="red" winner={false} imgURL={choices.rock}/>
+        <ChoiceCard title='Player' color="white" winner={true} imgURL={choices.paper}/>
         </div>
         <ul>
           <List />
--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function ChoiceCard(props) {
   return (
-    <div className={`choice-card ${props.color}`}>
-      <h3>{props.name}</h3>
-      <h1>BOX</h1>
+    <div className={`choice-card ${props.winner ? 'winner' : 'loser'}`}>
+      <h1>{props.title}</h1>
+      <img className='card-size' src={props.imgURL} alt={props.title} />
+      <h3>{props.winner ? 'Won' : 'Loss'}</h3>
     </div>
   );
 }
